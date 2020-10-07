4bd9c6c5a630ca8251a275a4d430ce49f407c7eb
jdfitzge <42559037+jdfitzge@users.noreply.github.com>
2020-10-07 09:45:06 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+#joshua fitzgerald 1374331
+
+mypassword = str(input())
+new_password = mypassword.replace('a','@')
+new_password = new_password.replace('m','M')
+new_password = new_password.replace('B','8')
+new_password = new_password.replace('o','.')
+new_password = new_password.replace('i','!')
+print(new_password + 'q*s')
