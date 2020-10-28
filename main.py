d0b1b1bbe01a92705982cc1b4158c633122e9381
jdfitzge <42559037+jdfitzge@users.noreply.github.com>
2020-10-28 10:26:31 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+#jdftizge 1374331
+integers = input().split()
+
+non_neg_int=[]
+
+for num in integers:
+    num = int(num)
+    if num >= 0:
+        non_neg_int.append(num)
+
+non_neg_int.sort()
+
+for i in non_neg_int:
+    print(i,end=' ')
